cd9e7814900d3166407ca135588c1785686b9bc1
Mandeep Singh <im@msingh.me>
2019-12-11 11:27:52 +0000

    update

--- emp/api/data.php
+++ emp/api/data.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 if(isset($_POST)){
 include '../../conn.php';
 $json = $_POST['data'];
@@ -7,7 +8,7 @@ $obj = json_decode($json, true);
 
 foreach ($obj as $values) {
 
-	foreach ($values as $value) {
+foreach ($values as $value) {
 
 $name = $value['name'];
 $email= $value['email'];
@@ -17,12 +18,8 @@ $phone= $value['phone'];
 $sql = "INSERT INTO employee (`name`,`email`,`password`,`phone`) VALUES ('".$name."','".$email."','".$password."','".$phone."')";
 
 $result = $conn->query($sql);
-//print_r($result);
 
-
-
-
-	}
+}
 	
 	
 
@@ -33,9 +30,9 @@ if ($result) {
 	$response_array['status'] = '200';
 	$response_array['message'] = 'New record created successfully';
     echo json_encode($response_array);
-	# code...
+	
 }else{
-    $response_array['status'] = '201';
+        $response_array['status'] = '201';
 	$response_array['message'] = 'New record not created';
     echo json_encode($response_array);
 }
